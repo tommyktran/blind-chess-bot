52c1bca715c7f45533da1e6f7a99c53579c1bd9f
Tommy Tran <tommyktran@gmail.com>
2021-02-02 20:34:34 -0500

    test

--- bot.js
+++ bot.js
@@ -4,24 +4,29 @@ const client = new Discord.Client();
 
 const prefix = "bc!"
 
+var fs = require('fs');
+
+var data = fs.readFileSync('lichess_db_puzzle.csv')
+    .toString() // convert Buffer to string
+    .split('\n') // split string to lines
+    .map(e => e.trim()) // remove white spaces for each line
+    .map(e => e.split(',').map(e => e.trim())); // split each line to array
+
 client.on('ready', () => {
 
     console.log('I am ready!');
 
 });
 
- 
+client.on('message', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
 
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
+    if (command == "puzzle") {
+        message.channel.send("test")
+    }
+}
  
 
 // THIS  MUST  BE  THIS  WAY
