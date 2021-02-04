5f94bb3202e1c8e654512d0d0aa8e0a358bb77fb
Tommy Tran <tommyktran@gmail.com>
2021-02-04 00:18:32 -0500

    lkemfw

--- bot.js
+++ bot.js
@@ -71,12 +71,12 @@ function goToMove(moves, number) {
         chess.move(moves[y])
     }
 }
-function clearPuzzle(index) {
-    message.channel.send(index)
-    message.channel.send(puzzles.length)
-    puzzles = puzzles.splice(index, 1)
-    chess.reset()
-}
+// function clearPuzzle(index) {
+//     message.channel.send(index)
+//     message.channel.send(puzzles.length)
+//     puzzles = puzzles.splice(index, 1)
+//     chess.reset()
+// }
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -87,13 +87,47 @@ client.on('message', async message => {
     if (command == "puzzle") {
         let messageArray = message.content.split(" ");
         let level = 3
-        if (messageArray.length == 2 && typeof parseInt(messageArray[1]) == "number" && !(messageArray[1].includes("-"))) {
-            level = parseInt(messageArray[1]);
+        let lowRating
+        let highRating
+        if (messageArray.length > 1) {
+            if (messageArray.length == 2 && !(isNaN(messageArray[1])) && !(messageArray[1].includes("-"))) {
+                level = parseInt(messageArray[1]);
+            } else if (messageArray.length == 2 && messageArray[1].includes("-")) {
+                if (!(isNaN(parseInt(messageArray[1].split("-")[0])) && !(isNaN(parseInt(messageArray[1].split("-")[1]))) && messageArray[1].split("-").length == 2)) {
+                    lowRating = parseInt(messageArray[1].split("-")[0])
+                    highRating = parseInt(messageArray[1].split("-")[1])
+                }
+            } else if (messageArray.length == 3) {
+                lowRating = parseInt(messageArray[1].split("-")[0])
+                highRating = parseInt(messageArray[1].split("-")[1])
+                level = parseInt(messageArray[2]);
+            }
         }
+        
         let movesBack = level * 2
         let movesToVisualize = []
         // Select a random puzzle from lichess_db_puzzle.csv
-        let puzzle = data[getRandomInt(data.length)]
+        // let puzzle = data[getRandomInt(data.length)]
+        let puzzle
+
+        if (lowRating < highRating) {
+            let tries = 0
+            let puzzleTemp = data[getRandomInt(data.length)]
+            while (tries < 100) {
+                if (puzzleTemp[3] >= lowRating && puzzleTemp[3] <= highRating) {
+                    puzzle = puzzleTemp
+                    tries += 100
+                } else {
+                    tries ++
+                    if (tries == 100) {
+                        message.channel.send("Puzzle not found in rating range")
+                        puzzle = data[getRandomInt(data.length)]
+                    }
+                }
+            }
+        } else {
+            puzzle = data[getRandomInt(data.length)]
+        }
 
         //0       ,1  ,2    ,3     ,4              ,5         ,6      ,7     ,8
         //PuzzleId,FEN,Moves,Rating,RatingDeviation,Popularity,NbPlays,Themes,GameUrl
@@ -271,7 +305,7 @@ client.on('message', async message => {
 
                         const embed = new Discord.MessageEmbed()
                             .setTitle('Keep Going')
-                            .setDescription("**" + puzzles[x].currentSolution.join(" ") + "**\n\n" + "Correct! Opponent responded with " + solutionArray[puzzles[x].solutionMove + 1] + ". What's the next move?")
+                            .setDescription("**" + puzzles[x].currentSolution.join(" ") + "**\n" + "Correct! Opponent responded with " + solutionArray[puzzles[x].solutionMove + 1] + ". What's the next move?")
                         
                         message.channel.send(embed);
 
@@ -282,7 +316,7 @@ client.on('message', async message => {
 
                         const embed = new Discord.MessageEmbed()
                             .setTitle('Puzzle Complete')
-                            .setDescription("**" + puzzles[x].currentSolution.join(" ") + "**\n\n" + "Correct! That's the end of the puzzle.")
+                            .setDescription("**" + puzzles[x].currentSolution.join(" ") + "**\n" + "Correct! That's the end of the puzzle.")
                         
                         message.channel.send(embed);
 
@@ -340,100 +374,6 @@ client.on('message', async message => {
             message.channel.send(embed);
             // message.channel.send("There is no puzzle currently active. Start one with `bc!puzzle`.")
         }
-
-
-
-        // let messageArray = message.content.split(" ");
-        // let move = messageArray[1]
-        // let puzzleInChannel = false
-        // let doneWithPuzzle = false
-        // for (x in puzzles) {
-        //     if (puzzles[x].message.channel == message.channel) {
-
-                // puzzleInChannel = true
-                // chess.load_pgn(puzzles[x].pgn, {sloppy: true})
-                // let moves = chess.history();
-
-                // chess.reset();
-
-        //         for (let y = 0; y < (puzzles[x].moveNumber + puzzles[x].movesBack + puzzles[x].solutionMove); y++) {
-        //             chess.move(moves[y]);
-        //         }
-
-                // let solutionArray = puzzles[x].puzzle[2].split(" ")
-                // solutionArray.shift()
-                // let solutionString = []
-                // for (y = 0; y < puzzles[x].solutionMove; y++) {
-                //     solutionString.push(chess.move(solutionArray[y], {sloppy: true}).san)
-                //     chess.undo()
-                //     message.channel.send(y)
-                //     message.channel.send(solutionArray[y])
-
-
-                //     message.channel.send(solutionString.join(" "))
-                // }
-                
-                // for (y = 0; y < puzzles[x].solutionMove - 1; y++) {
-                //     chess.move(solutionArray[y], {sloppy: true})
-                //     message.channel.send(chess.fen())
-                // }
-        //         message.channel.send(`\`\`\`"W"\n${puzzles[x].solutionMove}\n${solutionArray.length}\n${solutionArray.length -1}\n${solutionArray[(puzzles[x].solutionMove)]}\`\`\``)
-
-        //         let nextMove = chess.move(solutionArray[puzzles[x].solutionMove], {sloppy: true}).san
-
-        //         chess.undo()
-
-        //         let yourMove = chess.move(move, {sloppy: true})
-
-        //         if (typeof yourMove == "object" && yourMove !== null) {
-        //             yourMove = yourMove.san
-        //             chess.undo()
-        //         } else {
-        //             yourMove = "Invalid move"
-        //         }
-        //         message.channel.send(solutionArray.join(" "))
-        //         // message.channel.send(nextMove)
-        //         // message.channel.send(yourMove)
-        //         // message.channel.send(solutionArray[puzzles[x].solutionMove])
-        //         // message.channel.send(puzzles[x].solutionMove)
-
-                
-                
-        //         if (yourMove == nextMove) {
-        //             solutionString.push(nextMove)
-        //             solutionString = solutionString.join(" ")
-        //             message.channel.send("**" + solutionString + "**")
-        //             if (solutionArray.length - 1 == puzzles[x].solutionMove) {
-                        // message.channel.send("Correct! That's the end of the puzzle.")
-                        // doneWithPuzzle = true
-        //             } else {
-        //                 for (y = 0; y <= puzzles[x].solutionMove; y++) {
-        //                     chess.move(solutionArray[y], {sloppy: true})
-        //                 }
-                        // message.channel.send("Correct! Opponent responded with " + chess.move(solutionArray[(puzzles[x].solutionMove)+1], {sloppy: true}).san + ". What's the next move?")
-                        // puzzles[x].solutionMove += 2
-        //             }
-        //         } else {
-        //             if (solutionString.length != 0) {
-        //                 solutionString = solutionString.join(" ")
-        //                 message.channel.send("**" + solutionString + "**")
-        //             }
-        //             message.channel.send("Incorrect. Try again.")
-        //         }
-        //         chess.reset()
-        //     }
-        // }
-        // if (doneWithPuzzle == true) {
-        //     message.channel.send("LOL")
-        //     for (x in puzzles) {
-        //         if (puzzles[x].message.channel == message.channel) {
-        //             puzzles = puzzles.splice(x, 1)
-        //         }
-        //     }
-        // }
-        // if (puzzleInChannel == false) {
-            // message.channel.send("There is no puzzle currently active. Start one with `bc!puzzle`.")
-        // }
     }
 
     
