b3ff47a0d34ba29e25cdb52394b44df8168bdd54
Tommy Tran <tommyktran@gmail.com>
2022-02-09 20:03:17 -0500

    asdfasf

--- bot.js
+++ bot.js
@@ -256,7 +256,73 @@ client.on('message', async message => {
     
     
     }
+    function createTables(newdb) {
+        newdb.exec(`
+        create table hero (
+            hero_id int primary key not null,
+            hero_name text not null,
+            is_xman text not null,
+            was_snapped text not null
+        );
+        insert into hero (hero_id, hero_name, is_xman, was_snapped)
+            values (1, 'Spiderman', 'N', 'Y'),
+                   (2, 'Tony Stark', 'N', 'N'),
+                   (3, 'Jean Grey', 'Y', 'N');
     
+        create table hero_power (
+            hero_id int not null,
+            hero_power text not null
+        );
+    
+        insert into hero_power (hero_id, hero_power)
+            values (1, 'Web Slinging'),
+                   (1, 'Super Strength'),
+                   (1, 'Total Nerd'),
+                   (2, 'Total Nerd'),
+                   (3, 'Telepathic Manipulation'),
+                   (3, 'Astral Projection');
+            `, ()  => {
+                runQueries(newdb);
+        });
+    }
+
+    function runQueries(db) {
+        db.all(`
+        select hero_name, is_xman, was_snapped from hero h
+        inner join hero_power hp on h.hero_id = hp.hero_id
+        where hero_power = ?`, "Total Nerd", (err, rows) => {
+            rows.forEach(row => {
+                message.channel.send(row.hero_name + "\t" +
+                row.is_xman + "\t" +
+                row.was_snapped);
+            });
+        });
+    }
+    
+    function createDatabase() {
+        var newdb = new sqlite3.Database('mcu.db', (err) => {
+            if (err) {
+                console.log("Getting error " + err);
+                exit(1);
+            }
+            createTables(newdb);
+        });
+    }
+
+    if (command == "f") {
+        new sqlite3.Database('./mcu.db', sqlite3.OPEN_READWRITE, (err) => {
+            if (err && err.code == "SQLITE_CANTOPEN") {
+                createDatabase();
+                return;
+                } else if (err) {
+                    console.log("Getting error " + err);
+                    exit(1);
+            }
+            runQueries(db);
+        });
+
+    }
+
     if (command == "q") {
         thing++
         message.channel.send(thing)
